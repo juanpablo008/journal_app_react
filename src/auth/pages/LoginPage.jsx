ae398ae27c716284ff30e0167847f3911f6c92bc
Juan Pablo Bedoya <bedojuanpablo@gmail.com>
2023-01-20 16:16:41 -0500

    prop types dependency was added

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,42 +1,29 @@
-import { Link as RouterLink } from "react-router-dom";
-import { Google } from "@mui/icons-material"
-import { Button, Grid, Link, TextField, Typography } from "@mui/material"
+import { Link as RouterLink } from 'react-router-dom';
+import { Google } from '@mui/icons-material'
+import { Button, Grid, Link, TextField, Typography } from '@mui/material'
+import { AuthLayout } from '../layout/AuthLayout';
 
 export const LoginPage = () => {
   return (
-    <Grid 
-      container 
-      spacing={0} 
-      direction='column' 
-      alignItems='center' 
-      justifyContent='center'
-      sx={{ minHeight: '100vh', backgroundColor: 'primary.main', pa: 4}}
-    >
-
-    <Grid item
-      className='box-shadow'
-      xs={ 3 }
-      sx={{ backgroundColor: 'white', padding: 3, borderRadius: 2 }}
-    >
-      <Typography variant="h5" sx={{ mb: 1}}>Login</Typography>
+    <AuthLayout title='Login'>
       <form>
         <Grid container>
 
-          <Grid item xs={12} sx={{mt: 2}}>
-            <TextField 
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
               label='Correo'
               type='email'
               placeholder='correo@correo.com'
-              fullWidth 
+              fullWidth
             />
           </Grid>
 
-          <Grid item xs={12} sx={{mt: 2}}>
-            <TextField 
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
               label='Contraseña'
               type='password'
               placeholder='Contraseña'
-              fullWidth 
+              fullWidth
             />
           </Grid>
 
@@ -49,21 +36,19 @@ export const LoginPage = () => {
             <Grid item xs={12} sm={6}>
               <Button variant='contained' fullWidth>
                 <Google />
-                <Typography sx={{ml: 1}}>Google</Typography>
+                <Typography sx={{ ml: 1 }}>Google</Typography>
               </Button>
             </Grid>
           </Grid>
 
           <Grid container direction='row' justifyContent='end'>
-            <Link component={ RouterLink } color='inherit' to='/auth/register'>
+            <Link component={RouterLink} color='inherit' to='/auth/register'>
               Crear una cuenta
             </Link>
           </Grid>
 
         </Grid>
       </form>
-    </Grid>
-
-    </Grid>
+    </AuthLayout>
   )
 }
