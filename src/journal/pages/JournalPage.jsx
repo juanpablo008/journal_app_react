0f49cc6fef7a044a6d22422a942736725728bfa2
Juan Pablo Bedoya <bedojuanpablo@gmail.com>
2023-01-20 18:22:42 -0500

    Nothing selected view was created

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,11 @@
-import { Typography } from "@mui/material"
-import { JournalLayout } from "../layout/JournalLayout"
+import { JournalLayout } from '../layout/JournalLayout';
+import { NothingSelectedView } from '../views/';
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography >Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque blanditiis aut iste voluptatibus similique. Amet hic aut, animi quod architecto expedita debitis error doloremque illo, qui veritatis dolores at beatae.</Typography>
+      {/* <Typography >Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque blanditiis aut iste voluptatibus similique. Amet hic aut, animi quod architecto expedita debitis error doloremque illo, qui veritatis dolores at beatae.</Typography> */}
+      <NothingSelectedView />
     </JournalLayout>
   )
 }
