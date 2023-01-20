4b30d84fd0af9a070a27e2317edc7f3adb2ed7cd
Juan Pablo Bedoya <bedojuanpablo@gmail.com>
2023-01-20 16:05:14 -0500

    login design

--- src/theme/purpleTheme.js
+++ src/theme/purpleTheme.js
@@ -4,7 +4,7 @@ import { red } from "@mui/material/colors";
 export const purpleTheme = createTheme({
   palette:{
     primary: {
-      main: '#262225'
+      main: '#262254'
     },
     secondary: {
       main: '#543884'
